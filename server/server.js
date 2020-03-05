866a8b46a88bfdb5b5c147512dd26b48cfac1bb7
Stephenjoel2k <stephenjoel2k@hotmail.com>
2020-03-04 21:05:35 -0600

    removed cors from my code

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const app = express()
 const auth = require('./routes/auth.js')
 const queue = require('./routes/queue.js')
 
 //Middleware
 app.use(bodyParser.json())
-app.use(cors())
 
 //Routes
 app.use('/auth', auth)
